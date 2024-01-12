956d3d10085ef792f5780e80d9cbb0f37b087e19
Ivan Tymoshchuk <timohin2231@gmail.com>
2024-01-12 17:10:14 +0100

    del old libs swiper + add new and update

--- src/components/Donuts/Donuts.jsx
+++ src/components/Donuts/Donuts.jsx
@@ -1,68 +1,5 @@
-// import React from "react";
-// import { DonutsItem } from "./DonutsItem/DonutsItem";
-// import { data } from "./data";
-// import { Wraper, Title, List } from "./Donuts.styled";
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-
-// export const Donuts = () => {
-//   const settings = {
-//     dots: false,
-//     arrows: false,
-//     infinite: true,
-//     autoplay: true,
-//     adaptiveHeight: true,
-//     speed: 700,
-//     autoplaySpeed: 2000,
-//     slidesToShow: 1,
-//     cssEase: "linear",
-//     responsive: [
-//       {
-//         breakpoint: 1440,
-//         settings: {
-//           slidesToShow: 3,
-//         },
-//       },
-//       {
-//         breakpoint: 1024,
-//         settings: {
-//           slidesToShow: 2,
-//         },
-//       },
-//       {
-//         breakpoint: 600,
-//         settings: {
-//           slidesToShow: 2,
-//         },
-//       },
-//       {
-//         breakpoint: 480,
-//         settings: {
-//           slidesToShow: 1,
-//         },
-//       },
-//     ],
-//   };
-
-//   return (
-//     <Wraper>
-//       <Title>Bestsellers</Title>
-//       <List>
-//         <Slider {...settings}>
-//           {data.map((donuts, i) => {
-//             return <DonutsItem key={i} {...donuts} />;
-//           })}
-//         </Slider>
-//       </List>
-//     </Wraper>
-//   );
-// };
-
-// Donuts.js
-
 import React from 'react';
-import { Keyboard, Pagination } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 // import { Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Wraper, Title } from './Donuts.styled';
@@ -75,19 +12,40 @@ export const Donuts = () => {
       <Title>Bestsellers</Title>
 
       <Swiper
-        slidesPerView={3}
-        keyboard={{
-          enabled: true,
-        }}
+        slidesPerView={1}
         pagination={{
           clickable: true,
         }}
-        modules={[Keyboard, Pagination]}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        }}
+        modules={[Pagination]}
 
         //!autoplay --------------------
-        // slidesPerView={3}
+        // slidesPerView={1}
         // autoplay={{
         //   delay: 2000,
+        // }}
+        // breakpoints={{
+        //   640: {
+        //     slidesPerView: 1,
+        //   },
+        //   768: {
+        //     slidesPerView: 2,
+        //     spaceBetween: 20,
+        //   },
+        //   1024: {
+        //     slidesPerView: 3,
+        //   },
         // }}
         // modules={[Autoplay]}
         //!-------------------
