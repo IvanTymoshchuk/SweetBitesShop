5fd5972f804aa7dea3c18a6ea2223d35794f072c
Taras Senko <taras.senko.99@gmail.com>
2024-01-22 21:06:01 +0200

    add login validation functional

---
+++ src/utils/validations.js
@@ -0,0 +1,59 @@
+export function validationEmail(email){
+  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  if (emailRegex.test(email)) return {validation: true, message: ''}
+  return {validation: false, message: 'Некоректна email адреса'}
+}
+
+export function validationPassword(password){
+  let errorsList = [];
+  let validation = true;
+
+  let passwordValidDefinition =
+    [
+      {
+        minLength    : 8,
+        ErrorMessage : "Пароль повинен бути довшим за 8 символів",
+      },
+      {
+        maxLength    : 50,
+        ErrorMessage : "Пароль не повинен перевищувати 50 символів",
+      },
+      {
+        regex        : /.*\d/,
+        ErrorMessage : "Пароль повинен містити мінімум 1 цифру",
+      },
+      {
+        regex        : /.*[a-zA-Z]/,
+        ErrorMessage : "Пароль повинен містити мінімум 1 латинську букву",
+      },
+      {
+        regex        : /.*[!@#$%^&*() =+_-]/,
+        ErrorMessage : "Пароль повинен містити мінімум 1 спеціальний символ !@#$%^&*()=+_- або пробіл",
+      },
+    ];
+
+  for (let i = 0; i < passwordValidDefinition.length; i++)
+  {
+    let validator = passwordValidDefinition[i];
+    let valid = true;
+
+    if (validator.hasOwnProperty("regex"))
+    {
+      if (password.search(validator.regex) < 0) valid = false;
+    }
+
+    if (validator.hasOwnProperty("minLength"))
+    {
+      if (password.length < validator.minLength) valid = false;
+    }
+
+    if (validator.hasOwnProperty("maxLength"))
+    {
+      if (password.length > validator.maxLength) valid = false;
+    }
+
+    if (!valid) errorsList.push(validator.ErrorMessage);
+    if (!valid) validation = false;
+  }
+  return {validation: validation, message: errorsList}
+}
